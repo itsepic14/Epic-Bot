29d3b86dc0e5d96837a5c9fa5d909724ae3540b7
itsepic14 <thrilling4rowdy@gmail.com>
2021-05-27 10:39:56 +0530

    mute and unmute

--- main.js
+++ main.js
@@ -36,6 +36,10 @@ client.on('message', message =>{
         client.commands.get('kick').execute(message, args);
     } else if (command == 'ban'){
         client.commands.get('ban').execute(message, args);
+    } else if (command == 'mute'){
+        client.commands.get('mute').execute(message, args);
+    } else if (command == 'unmute'){
+        client.commands.get('unmute').execute(message, args);
     }
 });
 
